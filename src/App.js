c6b9ace4dcdd2ce481b161ccfe20554985a0e4ba
Glen Eggers <gleneggers64@gmail.com>
2021-07-10 14:27:19 -0500

    lift array state to parent App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import { ThemeProvider, CSSReset, Button } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
 import { Nav } from './components/Nav';
 import { Visualizer } from './components/Visualizer';
 import { Controller } from './components/Controller';
 
 function App() {
+  const [arrayBars, setArrayBars] = useState([]);
+  const [animations, setAnimations] = useState([]);
+  const [currentAnimation, setCurrentAnimation] = useState(0);
+
+  const generateArrayBars = () => {
+    const array = [];
+    for (let i = 0; i < 50; i++) {
+      array.push({
+        barHeight: Math.floor(Math.random() * 100)
+      })
+    }
+
+    return array;
+  };
+  
+  const generateAnimations = () => {
+    console.log(animations);
+  }
+
+  useEffect(() => {
+    setArrayBars(generateArrayBars());
+  }, [])
+
   return (
     <ThemeProvider>
       <div>
         <CSSReset />
         <Nav />
-        <Visualizer />
+        <Visualizer arrayBars={arrayBars} />
         <Controller />
       </div>
     </ThemeProvider>
--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,30 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Flex, Box, Button } from '@chakra-ui/react';
+import React from 'react';
+import { Flex, Box } from '@chakra-ui/react';
 
-export const Visualizer = () => {
-  const [arrayBars, setArrayBars] = useState([]);
-  const [animations, setAnimations] = useState([]);
-  const [currentAnimation, setCurrentAnimation] = useState(0);
-
-  const generateArrayBars = () => {
-    const array = [];
-    for (let i = 0; i < 50; i++) {
-      array.push({
-        barHeight: Math.floor(Math.random() * 100)
-      })
-    }
-
-    return array;
-  };
-  
-  const generateAnimations = () => {
-    console.log(animations);
-  }
-
-  useEffect(() => {
-    setArrayBars(generateArrayBars());
-  }, [])
-
+export const Visualizer = ({ arrayBars }) => {
   return (
     <Flex bg='tomato' h='65vh' w='80vw' justifyContent='space-between' mt='5vh' mb='5vh' mx='auto' alignItems='flex-end'>
       {arrayBars.map(bar => (<Box bg='teal' h={`${bar.barHeight}%`} w='10px' />))}
