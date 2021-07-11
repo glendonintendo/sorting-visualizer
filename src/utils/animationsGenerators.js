9038c9ed9923b4376720688e80ca1cf92207920c
Glen Eggers <gleneggers64@gmail.com>
2021-07-11 09:06:21 -0500

    add bubblesort animation generator

--- src/utils/animationsGenerators.js
+++ src/utils/animationsGenerators.js
@@ -1,5 +1,22 @@
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
+
 export const generateBubbleSortAnimations = (array) => {
   const animations = [];
+  const dupArray = [...array];
 
-  return animations = [];
+  let swapped;
+  for (let i = dupArray.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (dupArray[j].barHeight > dupArray[j + 1].barHeight) {
+        animations.push([j, j + 1]);
+        swap(dupArray, j, j + 1);
+        swapped = true;
+      }
+    }
+    if (swapped === false) break;
+  };
+
+  return animations;
 }
