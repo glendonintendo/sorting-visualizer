b07b5f0645dd56253cb46f849964f5b4c3c930e9
Glen Eggers <gleneggers64@gmail.com>
2021-07-15 11:32:16 -0500

    add icons for buttons; conditionally render play and pause on isPlaying

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,10 +1,47 @@
 import { Flex, Button } from "@chakra-ui/react";
+import {
+  IoPlaySkipBack,
+  IoChevronBack,
+  IoPlay,
+  IoPause,
+  IoChevronForward,
+  IoPlaySkipForward,
+} from "react-icons/io5";
 
-const Controller = ({ stepForwardAnimation, playAnimations }) => {
+const Controller = ({
+  stepForwardAnimation,
+  playAnimations,
+  pauseAnimations,
+  stepBackwardAnimation,
+  goToEnd,
+  goToStart,
+  isPlaying,
+}) => {
+  const playPauseButton = isPlaying ? (
+    <Button onClick={pauseAnimations}>
+      <IoPause />
+    </Button>
+  ) : (
+    <Button onClick={playAnimations}>
+      <IoPlay />
+    </Button>
+  );
+
   return (
     <Flex justifyContent="space-between" w="80vw" mx="auto">
-      <Button onClick={playAnimations}>Play</Button>
-      <Button onClick={stepForwardAnimation}>Step Forward</Button>
+      <Button onClick={goToStart}>
+        <IoPlaySkipBack />
+      </Button>
+      <Button onClick={stepBackwardAnimation}>
+        <IoChevronBack />
+      </Button>
+      {playPauseButton}
+      <Button onClick={stepForwardAnimation}>
+        <IoChevronForward />
+      </Button>
+      <Button onClick={goToEnd}>
+        <IoPlaySkipForward />
+      </Button>
     </Flex>
   );
 };
