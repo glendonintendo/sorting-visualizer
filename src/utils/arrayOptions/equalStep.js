9c696de8eeb7b011d193a3f25aa6de1ebb79dea5
Glen Eggers <gleneggers64@gmail.com>
2021-09-07 19:35:33 -0500

    fix step to make last bar 100% height

--- src/utils/arrayOptions/equalStep.js
+++ src/utils/arrayOptions/equalStep.js
@@ -1,10 +1,10 @@
 const generateEqualStepHeights = (array, arraySize) => {
   let currBarHeight = 5;
-  let step = 95 / arraySize;
+  let step = 95 / (arraySize - 1);
 
   for (let i = 0; i < arraySize; i++) {
     array.push({
-      barHeight: currBarHeight,
+      barHeight: currBarHeight.toFixed(2),
       color: "blue",
       key: i,
     });
