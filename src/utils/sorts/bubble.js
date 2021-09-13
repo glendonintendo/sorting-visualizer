20c0a8cc9aa35bc733132d7255e71f71fcfe9200
Glen Eggers <gleneggers64@gmail.com>
2021-09-13 10:14:25 -0500

    add optimized code for bubble sort

--- src/utils/sorts/bubble.js
+++ src/utils/sorts/bubble.js
@@ -5,7 +5,9 @@ const generateBubbleSortAnimations = (array) => {
   const animations = [];
   const dupArray = cloneArrayOfObjects(array);
 
+  let swapped = false;
   for (let i = dupArray.length - 1; i >= 0; i--) {
+    swapped = false;
     for (let j = 0; j < i; j++) {
       animations.push({
         type: "color",
@@ -15,6 +17,7 @@ const generateBubbleSortAnimations = (array) => {
       });
       if (dupArray[j].barHeight > dupArray[j + 1].barHeight) {
         animations.push({ type: "swap", idx1: j, idx2: j + 1 });
+        swapped = true;
         swap(dupArray, j, j + 1);
       }
       animations.push({
@@ -24,12 +27,24 @@ const generateBubbleSortAnimations = (array) => {
         newColors: ["blue", "blue"],
       });
     }
-    animations.push({
-      type: "color",
-      indeces: [i],
-      oldColors: ["blue"],
-      newColors: ["orange"],
-    });
+
+    if (swapped === false) {
+      animations.push({
+        type: "massColor",
+        startIdx: 0,
+        endIdx: i,
+        oldColor: "blue",
+        newColor: "orange",
+      });
+      break;
+    } else {
+      animations.push({
+        type: "color",
+        indeces: [i],
+        oldColors: ["blue"],
+        newColors: ["orange"],
+      });
+    }
   }
 
   return animations;
